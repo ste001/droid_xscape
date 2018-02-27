2d1ee648bda81c0c1cad50df8a083f2cabc94d7a
Stefano Merazzi <asso99@hotmail.com>
2018-02-27 17:18:43 +0100

    Added ship movement on the X axis.

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class PlayerController : MonoBehaviour {
+
+    [Tooltip("In ms^-1")][SerializeField] float xSpeed = 20f;
+    [Tooltip("In m")] [SerializeField]float xRange = 18f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        float xOffset = xThrow * xSpeed * Time.deltaTime;
+        float xRawPos = transform.localPosition.x + xOffset;
+        float clampedXPos = Mathf.Clamp(xRawPos, -xRange, xRange);
+
+        transform.localPosition = new Vector3
+            (clampedXPos, transform.localPosition.y, transform.localPosition.z);
+	}
+}
