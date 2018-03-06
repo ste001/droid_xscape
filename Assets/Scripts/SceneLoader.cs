257bb21062d8cfa9d1fbdf26709fdf00cea46119
Stefano Merazzi <asso99@hotmail.com>
2018-03-07 00:24:50 +0100

    Re-organized project, making sure everything is prefabbed.

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour {
+
+    // Use this for initialization
+    void Start()
+    {
+        Invoke("loadFirstScene", 2f);
+    }
+
+    void loadFirstScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
